16c43a2cc8b2bd4decf39ddccc6552f0de1bf034
Renan Coelho <sayhicoelho@gmail.com>
2020-06-09 00:41:34 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+exports.default = function isOdd(n) {
+  if (typeof n !== 'string') {
+    throw new Error('The value must be a string')
+  } else if (n.match(/\D/gi)) {
+    throw new Error('The value must have only digits')
+  }
+
+  return Number(String(n).split('').pop())
+    % 2 !== 0
+}
